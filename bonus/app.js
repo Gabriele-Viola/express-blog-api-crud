fa3752980f479155c69e535a69cf04ee9a465603
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-06 16:41:52 +0100

    add ind single element

--- bonus/app.js
+++ bonus/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const aliens = require('./database/aliens.js')
 const app = express()
 const PORT = process.env.PORT
 const HOST = process.env.HOST
+
 
 app.use(express.json())
 
@@ -9,3 +11,11 @@ app.listen(PORT, () => {
     console.log({success: `Your server works at ${HOST}:${PORT}`})
 })
 
+app.get('/aliens', (req, res) => {
+    res.status(200).json({data: aliens, count: aliens.lenght})
+})
+
+app.get('/aliens/:name', (req, res) => {
+   const alien = aliens.find (alien => alien.name.toLowerCase() === req.params.name)
+    res.status(404).json(alien)    
+})
